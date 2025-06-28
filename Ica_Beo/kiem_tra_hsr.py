4a4840064df62f166abefe0e6079feab378a5156
Chez_ <129508826+chezzakowo@users.noreply.github.com>
2025-06-28 22:18:36 +0700

    Đổi tên tiến trình

--- Ica_Beo/kiem_tra_hsr.py
+++ Ica_Beo/kiem_tra_hsr.py
@@ -2,6 +2,6 @@ import psutil
 
 def ktra_starrail():
     for process in psutil.process_iter(['pid', 'name']):
-        if process.info['name'] == "cmd.exe":
+        if process.info['name'] == "StarRail.exe":
             return True
     return False
